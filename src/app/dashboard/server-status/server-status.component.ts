f8ad0247ab9a79227254349f44f32945c98ec496
LuisFerVR <luis.ruiz@ex.gerdau.com>
2024-09-26 11:30:07 -0600

    Se agregó una suscripción a una señal con affect().

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { interval } from 'rxjs';
 
 @Component({
@@ -12,18 +12,25 @@ import { interval } from 'rxjs';
   }
 })
 export class ServerStatusComponent implements OnInit{
-  currentStatus : 'online' | 'offline' | 'unknown' = 'online';
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline');
+
   private destroyRef = inject(DestroyRef);
   
+  constructor(){
+    effect(()=>{
+      console.log(this.currentStatus());
+    });
+  }
+
   ngOnInit(){
     const interval = setInterval(()=>{
       let rnd = Math.random();
       if(rnd<0.5){
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       }else if(rnd<0.9){
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       }else{
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     },5000);
     this.destroyRef.onDestroy(()=>{
