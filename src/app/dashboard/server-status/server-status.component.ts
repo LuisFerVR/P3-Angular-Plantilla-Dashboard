6cc0acf93c0d116850e72e7ffbad017f96826edb
LuisFerVR <luis.ruiz@ex.gerdau.com>
2024-09-25 20:30:40 -0600

    Destruyendo el setInteval con ngOnDestroy

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -10,6 +10,23 @@ import { Component } from '@angular/core';
     class: 'status'
   }
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, OnDestroy{
+  currentStatus : 'online' | 'offline' | 'unknown' = 'online';
+  private interval ?: ReturnType<typeof setInterval>;
+
+  ngOnInit(){
+    setInterval(()=>{
+      let rnd = Math.random();
+      if(rnd<0.5){
+        this.currentStatus = 'online';
+      }else if(rnd<0.9){
+        this.currentStatus = 'offline';
+      }else{
+        this.currentStatus = 'unknown';
+      }
+    },5000);
+  }
+  ngOnDestroy(){
+    clearTimeout(this.interval);
+  }
 }
