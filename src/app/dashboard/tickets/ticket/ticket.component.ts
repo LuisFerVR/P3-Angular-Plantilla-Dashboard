fa6ff1214e8732552bf5b0bf54920adb9c1ab6b5
LuisFerVR <luis.ruiz@ex.gerdau.com>
2024-09-26 20:53:55 -0600

    Se agregó uun método a ticket para expandir los detalles del ticket mediante un metodo que usa señales y el método update para actualizarlas.

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
+  detailsVisible = signal(false);
 
+  onToggleDetails(){
+    //Actualizar una señal sin método update de las señales
+    //this.detailsVisible.set(!this.detailsVisible());
+    //Alternativa con método update
+    this.detailsVisible.update((wasVisible)=>!wasVisible);
+  }
 }
